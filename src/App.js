e46545bef636aa0147bcaaa18122ac7857ee7a0c
Barrettnj0 <63025815+Barrettnj0@users.noreply.github.com>
2022-03-15 23:48:00 -0500

    Fix for pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+// React imports
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes as Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Calculators from "./Components/Calculators";
 
+// Stylesheet
+import './Stylesheets/App.css';
+
+// Website Routing Functionality
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='router'>
+        <nav className='navbar'>
+          <ul className='navbar-listings'>
+              <Link className='link' to="/">Home</Link>
+              <Link className='link' to="/calculators">DPS Calculators</Link>
+              <Link className='link' to="/users">Users</Link>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/calculators" element={ <Calculators/> } />
+          <Route path="/users" element={ <Users/> } />
+          <Route path="/" element={ <Home/> } />
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 export default App;
